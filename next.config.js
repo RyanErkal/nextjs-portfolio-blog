eef6a8d2bf5805eb102b60c0b15b7f4d6f972623
Ryan Erkal <ryanerkal@yahoo.ie>
2023-11-23 09:12:04 +0000

    env

--- next.config.js
+++ next.config.js
@@ -12,9 +12,8 @@ const nextConfig = {
 		]
 	},
 	env: {
-		NEXT_PUBLIC_CONTENTFUL_ACCESS_TOKEN:
-			process.env.NEXT_PUBLIC_CONTENTFUL_ACCESS_TOKEN,
-		NEXT_PUBLIC_SPACE_ID: process.env.NEXT_PUBLIC_SPACE_ID
+		CONTENTFUL_ACCESS_TOKEN: process.env.CONTENTFUL_ACCESS_TOKEN,
+		SPACE_ID: process.env.SPACE_ID
 	}
 };
 
