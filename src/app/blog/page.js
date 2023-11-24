da7b87caa7698ba4e47aeef26adf5b4fe9a4833f
Ryan Erkal <ryanerkal@yahoo.ie>
2023-11-24 11:45:35 +0000

    load more

--- src/app/blog/page.js
+++ src/app/blog/page.js
@@ -1,6 +1,6 @@
 "use client";
 
-import { useEffect, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import { contentfulApiGQL } from "../api/contentful";
 import Nav from "./components/Nav";
 import Post from "./components/Post";
@@ -9,6 +9,7 @@ import Footer from "../components/Footer";
 export default function Blog() {
 	const [posts, setPosts] = useState();
 	const [load, setLoad] = useState(10);
+	const total = useRef();
 
 	useEffect(() => {
 		/* getEntries().then((response) => {
@@ -37,18 +38,21 @@ export default function Blog() {
 			}
 		  }`)().then((response) => {
 			setPosts(response.data.blogPostCollection.items);
+			total.current = response.data.blogPostCollection.total;
 		});
 	}, [load]);
 
 	const handleLoadMore = () => {
-		setLoad(load + 10);
+		if (!(load > total.current)) {
+			setLoad(load + 10);
+		}
 	};
 
 	return (
 		<div className="bg-slate-800 min-h-screen h-full w-screen overflow-hidden flex flex-col justify-between">
 			<Nav />
 			<div className="flex flex-col items-center">
-				<div className="p-2 pb-8 relative grid grid-cols-1 lg:grid-cols-2 lg:gap-8">
+				<div className="p-2 pb-4 relative grid grid-cols-1 lg:grid-cols-2 lg:gap-8">
 					{!posts ? (
 						<div className="h-screen">Loading...</div>
 					) : (
@@ -60,7 +64,7 @@ export default function Blog() {
 			</div>
 			<div className="flex justify-center relative">
 				<button
-					className="bg-slate-700 text-white px-4 py-2 mb-8 rounded-xl hover:bg-slate-600 transition-all"
+					className="bg-slate-700 text-white px-4 py-2 mx-8 mb-4 w-full lg:w-fit rounded-xl hover:bg-slate-600 transition-all"
 					onClick={handleLoadMore}>
 					Load More
 				</button>
